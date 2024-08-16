19493d970c36575048edc4f0a193a6c8826f2744
Roderick <roypanatec@gmail.com>
2024-08-16 13:13:06 -0500

    login and dashboard template done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/login';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
